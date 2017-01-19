9c67a81c151bcc01673d2e0c0bcc196e64e7755a
Jason Dobry <jason.dobry@gmail.com>
2017-01-18 16:31:35 -0800

    Allow exception for exclamation point in title.

--- test/utils.js
+++ test/utils.js
@@ -18,7 +18,7 @@ var path = require('path');
 exports.TUTORIALS_PATH = path.join(__dirname, '/../tutorials');
 
 exports.FILENAME_REGEXP = /^[a-z0-9\-]+\.md$/; // e.g. setting-up-postgres.md
-exports.TITLE_REGEXP = /^[a-zA-Z0-9\s\.\-\(\)&]+$/; // e.g. How to Set Up PostgreSQL on Compute Engine
+exports.TITLE_REGEXP = /^[a-zA-Z0-9\s\.\-\(\)&!]+$/; // e.g. How to Set Up PostgreSQL on Compute Engine
 exports.DESCRIPTION_REGEXP = /^[a-zA-Z0-9\s\.,\-\(\)&]+$/; // e.g. Learn how to get PostgreSQL running on Compute Engine
 exports.GITHUB_REGEXP = /^[a-zA-Z0-9\-]+$/; // e.g. jimtravisgoog
 exports.TAGS_REGEXP = /^[a-zA-Z0-9\.,\s\-]+$/; // e.g. Compute Engine, PostgreSQL
