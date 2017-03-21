4b7a544ddf1925677f5ed6c11e32a368e32f88e5
Mike McDonald <mpmcdonald@mpmcdonald-macbookpro.roam.corp.google.com>
2017-03-20 23:57:18 -0700

    Too enthusiastic

--- tutorials/run-swift-vapor-on-google-cloud/main.swift
+++ tutorials/run-swift-vapor-on-google-cloud/main.swift
@@ -12,7 +12,7 @@ drop.get("/_ah/health") { request in
 // Basic GET request
 drop.get("/hello") { request in
     print("GET - /hello route handler...")
-    return "Hello from Swift on GAE Flex!!"
+    return "Hello from Swift on GAE Flex!"
 }
 
 // Start server on 8080 (default)
