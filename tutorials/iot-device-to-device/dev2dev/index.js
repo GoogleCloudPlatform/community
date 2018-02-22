4ad13cfa39f0340f72785ef0720aaa9b09e0a91b
Gus Class <gguuss@gmail.com>
2018-02-21 17:25:56 -0800

    Updates ADC boilerplate.

--- tutorials/iot-device-to-device/dev2dev/index.js
+++ tutorials/iot-device-to-device/dev2dev/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const fs = require('fs');
 const google = require('googleapis');
 
 const API_VERSION = 'v1';
@@ -13,10 +12,6 @@ function getClient (serviceAccountJson, cb) {
     if (err) {
       console.log('Authentication failed because of ', err);
       return;
-    }
-    if (authClient.createScopedRequired && authClient.createScopedRequired()) {
-      var scopes = ['https://www.googleapis.com/auth/cloud-platform'];
-      client = authClient.createScoped(scopes);
     }
 
     google.options({auth: authClient});
@@ -32,7 +27,7 @@ function getClient (serviceAccountJson, cb) {
 
 // From nodejs-docs-samples/iot/manager.js#605...636
 function setDeviceConfig (client, deviceId, registryId, projectId,
-    cloudRegion, data, version) {
+  cloudRegion, data, version) {
   const parentName = `projects/${projectId}/locations/${cloudRegion}`;
   const registryName = `${parentName}/registries/${registryId}`;
 
@@ -46,27 +41,27 @@ function setDeviceConfig (client, deviceId, registryId, projectId,
   console.log('Set device config.');
 
   client.projects.locations.registries.devices.modifyCloudToDeviceConfig(
-      request,
-      (err, data) => {
-        if (err) {
-          console.log('Could not update config:', deviceId);
-          console.log('Message: ', err);
-        } else {
-          console.log('Success :', data);
-        }
-      });
+    request,
+    (err, data) => {
+      if (err) {
+        console.log('Could not update config:', deviceId);
+        console.log('Message: ', err);
+      } else {
+        console.log('Success :', data);
+      }
+    });
 }
 
 exports.relayCloudIot = function (event, callback) {
   // [START iot_relay_message_js]
   const pubsubMessage = event.data;
   const record = JSON.parse(
-      pubsubMessage.data ?
-          Buffer.from(pubsubMessage.data, 'base64').toString() :
-          '{}');
+    pubsubMessage.data
+      ? Buffer.from(pubsubMessage.data, 'base64').toString()
+      : '{}');
 
   let messagesSent = record.hops;
-  messagesSent = messagesSent+1;
+  messagesSent = messagesSent + 1;
   console.log(`${record.deviceId} ${record.registryId} ${messagesSent}`);
 
   const config = {
@@ -78,11 +73,10 @@ exports.relayCloudIot = function (event, callback) {
 
   const cb = function (client) {
     setDeviceConfig(client, record.deviceId, record.registryId,
-        process.env.GCLOUD_PROJECT || process.env.GOOGLE_CLOUD_PROJECT,
-        record.cloudRegion, JSON.stringify(config), 0);
+      process.env.GCLOUD_PROJECT || process.env.GOOGLE_CLOUD_PROJECT,
+      record.cloudRegion, JSON.stringify(config), 0);
   };
 
   getClient(process.env.GOOGLE_APPLICATION_CREDENTIALS, cb);
   // [END iot_relay_message_js]
 };
-
