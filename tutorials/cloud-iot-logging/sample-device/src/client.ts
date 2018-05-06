9266e0e7935c621a90827d31d6cfee6bde71c572
Preston Holmes <preston@ptone.com>
2018-05-06 08:59:04 -0700

    update license holder, clean up some comment cruft

--- tutorials/cloud-iot-logging/sample-device/src/client.ts
+++ tutorials/cloud-iot-logging/sample-device/src/client.ts
@@ -1,25 +1,25 @@
-/**
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+# Copyright Google Inc. 2018
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
+
 
 import mqtt = require('mqtt');
 import { TokenGenerator, SignAlgorithm } from "./token";
 import { setInterval } from 'timers';
 import { Observable } from 'rxjs';
 import 'rxjs/add/observable/fromEvent';
-// import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {ReplaySubject} from 'rxjs';
 
 export class IoTClient {
@@ -34,9 +34,6 @@ export class IoTClient {
     messages$: ReplaySubject<any>;
     // tslint:disable-next-line: no-any
     publishConfirmations$: ReplaySubject<any>;
-
-    // private _connected = new BehaviorSubject(new mqtt.IConnackPacket());
-    // public readonly connected$: Observable<mqtt.IConnackPacket> = this._connected.asObservable();
 
     constructor (
         private projectId: string,
@@ -90,13 +87,10 @@ export class IoTClient {
                 }
             });
             this.connections$.next(connack);
-
-
         });
             
         this.client.on('error', (error) =>
             console.error(error));
-        
     }
     
     publish(topic: string, payload: string | Buffer) {
--- tutorials/cloud-iot-logging/sample-device/src/index.ts
+++ tutorials/cloud-iot-logging/sample-device/src/index.ts
@@ -1,22 +1,21 @@
-/**
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+# Copyright Google Inc. 2018
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
 
 import { IoTClient } from './client';
 import { SignAlgorithm } from './token';
-// import "rxjs/add/operator/first";
 import { first } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import 'rxjs/add/observable/timer';
@@ -46,6 +45,7 @@ const client = new IoTClient(
     20
 );
 
+// uncomment if you want to see that publish messages are getting through
 // client.publishConfirmations$.subscribe(ack => console.log("message published"));
 
 client.messages$.subscribe(msg => {
@@ -69,8 +69,8 @@ client.messages$.subscribe(msg => {
         }
     }
 });
-let clientConnected = false;
 
+let clientConnected = false;
 
 const initialConnect = client.connections$.pipe(first());
 
