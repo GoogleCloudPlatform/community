888433de00c6c04d079a917a284696157e71f050
Preston Holmes <preston@ptone.com>
2018-05-11 17:43:31 -0700

    basic functionality

---
+++ tutorials/cloud-iot-firestore-config/functions/src/devices.ts
@@ -0,0 +1,175 @@
+const {google} = require('googleapis')
+interface Credential {};
+
+interface Device {
+  id: string;
+  name?: string;
+  readonly numId?: string;
+  credentials: Credential[];
+  "config": any[];
+
+  // many of the following fields could be added to the device interface:
+
+  // "lastHeartbeatTime": string,
+  // "lastEventTime": string,
+  // "lastStateTime": string,
+  // "lastConfigAckTime": string,
+  // "lastConfigSendTime": string,
+  // "blocked": boolean,
+  // "lastErrorTime": string,
+  // "lastErrorStatus": {
+  //   object(Status)
+  // },
+  // {
+  //   object(DeviceConfig)
+  // },
+  // "state": {
+  //   object(DeviceState)
+  // },
+  // "metadata": {
+  //   string: string,
+  //   ...
+  // },
+}
+
+export class DeviceManager {
+  client: any;
+  ready:Boolean = false;
+  private parentName: string = '';
+  private registryName: string = '';
+
+  setClient(err: Error, hClient: any, project: string) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    let aClient = hClient;
+    const scopes = ['https://www.googleapis.com/auth/cloud-platform'];
+    if (hClient.createScopedRequired && hClient.createScopedRequired()) {
+      // Scopes can be specified either as an array or as a single, space-delimited string.
+      aClient = hClient.createScoped(scopes);
+    }
+    this.client = google.cloudiot({
+      version: 'v1',
+      auth: aClient
+    });
+    this.parentName = `projects/${project}/locations/${this.region}`;
+    this.registryName = `${this.parentName}/registries/${this.registryId}`
+    this.ready = true;
+  }
+
+  constructor(private registryId:string, private region:string='us-central1') {
+    google.auth.getApplicationDefault(this.setClient.bind(this))
+  }
+  
+  createDevice(device:any) {
+    return new Promise((resolve, reject) => {
+      const request = {
+        parent: this.registryName,
+        resource: device
+      }
+      this.client.projects.locations.registries.devices.create(request, (err:any, data:any) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        } else {
+          // console.log('device created');
+          resolve(data);
+        }
+      });
+    });
+  }
+
+  updateDevice(deviceId:string, device:any, updateMask?:any) {
+    return new Promise((resolve, reject) => {
+      let request = {
+        name:`${this.registryName}/devices/${deviceId}`,
+        resource: device,
+      }
+      if (updateMask) {
+        // tslint:disable-next-line: no-any
+        (request as any)['updateMask'] = updateMask;
+      }
+      this.client.projects.locations.registries.devices.patch(request, (err:any, resp:any) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        } else {
+          resolve(resp.data);
+        };
+      }); 
+    });
+  }
+
+  deleteDevice(deviceId:string) {
+    return new Promise((resolve, reject) => {
+      console.log("delete"); 
+      this.client.projects.locations.registries.devices.delete({name:`${this.registryName}/devices/${deviceId}`}, (err:any, resp:any) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        } else {
+          resolve(resp.data);
+        };
+      }); 
+    });
+  }
+
+  // sendConfig(deviceId:string, config) {}
+
+  //getState(deviceId:string) {}
+
+  getDevice(deviceId:string) {
+    return new Promise((resolve, reject) => {
+      this.client.projects.locations.registries.devices.get({name:`${this.registryName}/devices/${deviceId}`}, (err:any, resp:any) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        } else {
+          resolve(resp.data);
+        };
+      }); 
+    });
+  }
+
+  listDevices(pageToken?:string) {
+    return new Promise((resolve, reject) => {
+      const request: any = {
+        parent: this.registryName,
+        //resource: body
+        pageSize: 50,
+      };
+      if (pageToken) {
+        request['pageToken'] = pageToken;
+      }
+      // console.log(request);
+      // console.log(this.client);
+      this.client.projects.locations.registries.devices.list(request, (err:any, resp:any) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        } else {
+          resolve(resp.data);
+        };
+      });
+    });
+  }
+
+  updateConfig(deviceId:string, config:any) {
+    return new Promise((resolve, reject) => {
+      let request = {
+        name:`${this.registryName}/devices/${deviceId}`,
+        binaryData: Buffer.from(JSON.stringify(config)).toString("base64"),
+      }
+      this.client.projects.locations.registries.devices.modifyCloudToDeviceConfig(request, (err:any, resp:any) => {
+        if (err) {
+          console.error(err);
+          return reject(err);
+        } else {
+          resolve(resp.data);
+        };
+      });
+    });
+  }
+
+}
---
+++ tutorials/cloud-iot-firestore-config/functions/src/index.ts
@@ -0,0 +1,42 @@
+/*
+# Copyright Google Inc. 2018
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
+
+
+'use strict';
+
+import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions';
+import { runInDebugContext } from 'vm';
+import { DeviceManager } from './devices';
+
+// create a device manager instance with a registry id, optionally pass a region
+const dm = new DeviceManager('config-demo');
+
+// start cloud function
+exports.configUpdate = functions.firestore
+  // assumes a document whose ID is the same as the deviceid
+  .document('device-config/{deviceId}')
+  .onWrite((change: functions.Change<admin.firestore.DocumentSnapshot>, context?: functions.EventContext) => {
+    if (context) {
+      console.log(context.params.deviceId);
+      // get the new config data
+      const configData = change.after.data();
+      return dm.updateConfig(context.params.deviceId, configData);
+    } else {
+      throw(Error("no context from trigger"));
+    }
+
+  })
