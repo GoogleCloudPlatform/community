031fbb685c649f86e720470824746d0152e7e892
Preston Holmes <preston@ptone.com>
2018-05-18 18:08:00 -0700

    handle binary payload in the device

--- tutorials/cloud-iot-firestore-config/sample-device/src/index.ts
+++ tutorials/cloud-iot-firestore-config/sample-device/src/index.ts
@@ -20,11 +20,21 @@ import { first } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import 'rxjs/add/observable/timer';
 import 'rxjs/add/observable/merge';
+import cbor = require('cbor');
 
 if (!process.env.GCLOUD_PROJECT) {
     console.error("Error: GCLOUD_PROJECT env variable unset");
     process.exit(1);
 }
+
+let useBinary = false;
+let deviceId = 'sample-device';
+if (process.argv.length > 2 && process.argv[2] == '-b') {
+    useBinary = true;
+    deviceId = 'sample-binary';
+    console.log("Device uses binary config");
+}
+
 const client = new IoTClient(
     // projectId: 
     process.env.GCLOUD_PROJECT as string,
@@ -34,7 +44,7 @@ const client = new IoTClient(
     // '<set to your registry id>',
     process.env.REGISTRY_ID as string,
     // deviceId: 
-    'sample-device',
+    deviceId,
     // privateKeyFile: 
     './ec_private.pem',
     // algorithm: 
@@ -48,18 +58,25 @@ const client = new IoTClient(
 // uncomment if you want to see that publish messages are getting through
 // client.publishConfirmations$.subscribe(ack => console.log("message published"));
 
+
 client.messages$.subscribe(msg => {
-    const msgContent = Buffer.from(msg, 'base64').toString();
     let config;
-    try {
-        config = JSON.parse(msgContent);
-        console.log("Current Config: ");
-        console.log(config);
+    if (useBinary) {
+        config = cbor.decodeFirstSync(msg);
+
+    } else {
+        const msgContent = Buffer.from(msg, 'base64').toString();
+        try {
+            config = JSON.parse(msgContent);
+        }
+        catch (e) {
+            console.error("latest config not valid json");
+            return;
+        }
     }
-    catch (e) {
-        console.error("latest config not valid json");
-        return;
-    }
+    console.log("Current Config: ");
+    console.log(config);
+
 });
 
 let clientConnected = false;
