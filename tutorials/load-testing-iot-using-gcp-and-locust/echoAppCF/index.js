9295a4e1b979bc58aaa4b7f7d8668241156b92f6
Chris Scott <scchristopher@google.com>
2019-02-04 12:26:38 -0700

    fix linter errore

--- tutorials/load-testing-iot-using-gcp-and-locust/echoAppCF/index.js
+++ tutorials/load-testing-iot-using-gcp-and-locust/echoAppCF/index.js
@@ -9,71 +9,71 @@ const discoveryUrl = `${DISCOVERY_API}?version=${API_VERSION}`;
 
 // Sends a command to a device.
 function sendCommand(client, deviceId, registryId, projectId, region, command) {
-    const parentName = `projects/${projectId}/locations/${region}`;
-    const registryName = `${parentName}/registries/${registryId}`;
-    const binaryData = Buffer.from(command).toString('base64');
-    const request = {
-        name: `${registryName}/devices/${deviceId}`,
-        binaryData: binaryData
-    };
+  const parentName = `projects/${projectId}/locations/${region}`;
+  const registryName = `${parentName}/registries/${registryId}`;
+  const binaryData = Buffer.from(command).toString('base64');
+  const request = {
+    name: `${registryName}/devices/${deviceId}`,
+    binaryData: binaryData
+  };
 
-    return new Promise((resolve, reject) => {
-        client.projects.locations.registries.devices.sendCommandToDevice(request,
-            (err, data) => {
-                if (err) {
-                    console.log('*** error sending command', err);
-                    reject(err);
-                } else {
-                    resolve('ok');
-                }
-        });
+  return new Promise((resolve, reject) => {
+    client.projects.locations.registries.devices.sendCommandToDevice(request,
+      (err, data) => {
+        if (err) {
+          console.log('*** error sending command', err);
+          reject(err);
+        } else {
+          resolve('ok');
+        }
     });
+  });
 }
 
 exports.echoAppCF = async (data, context) => {
 
-    console.log('*** data=', data);
+  console.log('*** data=', data);
 
-    // extract attributes needed for sendCommand call
-    const deviceAttributes = data.attributes;
-    const deviceId = deviceAttributes.deviceId;
-    const registryId = deviceAttributes.deviceRegistryId;
-    const projectId = deviceAttributes.projectId;
-    const region = deviceAttributes.deviceRegistryLocation;
+  // extract attributes needed for sendCommand call
+  const deviceAttributes = data.attributes;
+  const deviceId = deviceAttributes.deviceId;
+  const registryId = deviceAttributes.deviceRegistryId;
+  const projectId = deviceAttributes.projectId;
+  const region = deviceAttributes.deviceRegistryLocation;
 
-    try {
-        // build message to send back
-        const deviceMessage = Buffer.from(data.data, 'base64').toString();
-        console.log('*** received', deviceMessage);
+  try {
+    // build message to send back
+    const deviceMessage = Buffer.from(data.data, 'base64').toString();
+    console.log('*** received', deviceMessage);
 
-        const messageToSend = deviceMessage + ' ack';
+    const messageToSend = deviceMessage + ' ack';
 
-        // get the auth client
-        let authClient = await google.auth.getClient().catch((error) => {
-            console.log('*** caught error on getClient', error);
-            throw new Error('getClient');
-        });
+    // get the auth client
+    let authClient = await google.auth.getClient().catch((error) => {
+      console.log('*** caught error on getClient', error);
+      throw new Error('getClient');
+    });
 
-        // set auth
-        google.options({
-            auth: authClient
-        });
+    // set auth
+    google.options({
+      auth: authClient
+    });
 
-        // get API client
-        const apiClient = await google.discoverAPI(discoveryUrl).catch((error) => {
-            console.log('*** caught error on discoverAPI', error);
-            throw new Error('discoverAPI');
-        });
+    // get API client
+    const apiClient = await google.discoverAPI(discoveryUrl).catch((error) => {
+      console.log('*** caught error on discoverAPI', error);
+      throw new Error('discoverAPI');
+    });
 
-        // send the command to the device
-        console.log(`*** sending ${messageToSend}`);
-        await sendCommand(apiClient, deviceId, registryId, projectId, region, messageToSend).catch((error) => {
-            console.log('*** caught error on sendCommand', error);
-            throw new Error('sendCommand');
-        });
-    }
-    catch (error) {
-        console.log('*** caught error', error);
-    }
+    // send the command to the device
+    console.log(`*** sending ${messageToSend}`);
+    await sendCommand(apiClient, deviceId, registryId, projectId, region, messageToSend).catch((error) => {
+      console.log('*** caught error on sendCommand', error);
+      throw new Error('sendCommand');
+    });
+  }
+  catch (error) {
+    console.log('*** caught error', error);
+  }
 
 };
