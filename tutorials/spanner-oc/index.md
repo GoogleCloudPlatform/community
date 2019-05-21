3499443dbbbb783101778317eb952800215474e9
Mayur Kale <mayurkale@google.com>
2019-05-20 17:00:58 -0700

    OpenCensus for Spanner

---
+++ tutorials/spanner-oc/index.md
@@ -0,0 +1,141 @@
+---
+title: Using OpenCensus with Cloud Spanner and Stackdriver Trace and Monitoring
+description: Implement client-side tracing in Cloud Spanner with OpenCensus and Stackdriver.
+author: mayurkale, rghetia
+tags: Cloud Spanner, OpenCensus, Stackdriver, tracing, stats
+date_published: 2019-05-21
+---
+
+This tutorial shows how to implement client-side tracing in your Cloud Spanner workloads 
+using OpenCensus and Stackdriver. While Cloud Spanner surfaces a number of helpful server-side 
+metrics with Stackdriver, applications can realize added benefits from implementing client-side tracing.
+For example, server-side metrics do not give you a window into the round-trip latency of calls made to your 
+Cloud Spanner endpoint, which can only be surfaced using client-side tracing.
+
+[Cloud Spanner](https://cloud.google.com/spanner/) is the first scalable, enterprise-grade, globally-distributed, and strongly consistent database service built for the cloud specifically to combine the benefits of relational database structure with non-relational horizontal scale.
+
+[OpenCensus](https://opencensus.io) is an open source library that can be used to provide observability in your applications. It is vendor-agnostic and integrates with a number of backends such as Prometheus and Zipkin. In this tutorial, we use Stackdriver as the tracing backend.
+
+
+## Costs
+
+This tutorial uses the following billable components of Google Cloud Platform (GCP):
+
+*   Google Compute Engine
+*   Cloud Spanner
+*   Stackdriver
+
+You can use the [Pricing Calculator](https://cloud.google.com/products/calculator/) to generate a cost
+estimate based on your projected usage.
+
+New GCP users might be eligible for a [free trial](https://cloud.google.com/free/).
+
+We recommend that you deploy this tutorial into an ephemeral project, which can then be deleted once you’re done.
+
+## Before you begin
+
+### Create a new project
+
+1.  In the GCP Console, go to the [Manage resources page](https://console.cloud.google.com/cloud-resource-manager).
+2.  Select a project, or click **Create Project** to create a new GCP project.
+3.  In the dialog, name your project. Make a note of your generated project ID.
+4.  Click **Create** to create a new project.    
+
+## Creating a Cloud Spanner instance
+
+In this section, you will create a new Cloud Spanner instance that will be used later by our Java application.
+
+In Cloud Shell, create a Cloud Spanner instance in the region us-central1 with 1 node:
+
+    gcloud spanner instances create test-instance 
+      --config=regional-us-central1 \
+      --description="Test Instance" 
+      --nodes=1
+
+This command might take a few minutes to complete.
+      
+Note that this creates an instance with the following characteristics:
+1. Instance ID `test-instance`
+2. Display name `Test Instance`
+3. Instance configuration `regional-us-central1`
+4. Node count of 1 (`node_count` corresponds to the amount of serving and storage resources available to databases in the instance.)
+
+## Create and configure a Compute Engine VM
+
+Create a Compute Engine VM with the necessary security scopes by running the following commands from Cloud Shell. 
+
+    gcloud compute instances create trace-client  --zone=us-central1-c \
+    --scopes="https://www.googleapis.com/auth/spanner.admin.table,\
+    https://www.googleapis.com/auth/spanner.data,\
+    https://www.googleapis.com/auth/logging.write,\
+    https://www.googleapis.com/auth/monitoring.write,\
+    https://www.googleapis.com/auth/trace.append"
+
+## Deploying the Java application
+
+In this section, you download the Java application containing the instrumented code, make the necessary modifications to reflect your environment and then run it.
+
+1.  Log in to the VM
+
+        gcloud compute ssh trace-client --zone=us-central1-c
+        
+1.  Run the following command to install Git, the Java 8 JDK, and Maven:
+
+        sudo apt-get install git openjdk-8-jdk maven -y
+
+1.  Clone the source repository for this tutorial:
+
+        git clone https://github.com/GoogleCloudPlatform/community.git
+        
+Update the Java application with some configuration specific to your project.
+
+1.  Navigate to the folder containing the Java source:
+
+        cd community/tutorials/spanner-oc/java/
+
+1.  Configure the environment variable for the application code to use the `spanner-oc` Spanner instance and `demo-1` as database id:
+
+        export INSTANCE_ID=spanner-oc
+        export DATABASE_ID=demo-1
+        
+1.  Run the following maven commands to build and run the program:
+
+        mvn package -DskipTests --quiet
+        mvn exec:java -Dexec.mainClass=com.example.spanner.App --quiet
+
+## Viewing traces in Stackdriver Trace UI
+
+As soon as the code ran, it started exporting to Stackdriver.
+
+1.  Go to the [Stackdriver Trace console](https://cloud.google.com/console/traces).
+
+1.  If you select **create-players**, you'll be taken to a view 
+that shows more information about the call chain, along with other useful 
+information such as lower-level tracing and operation-level call latencies.
+
+    Drilling down to a particular data point in time yields a trace with a root span and children spans shown below:
+
+    ![Screenshot](./images/trace-timeline.png)
+
+1.  The power of visualization helps teams perform root cause analyses, post-mortems, bisect bad changes, figure out what needs to be optimized.
+
+1.  Please note that for this demo we are always sampling and that is very high. More realistically perhaps tracing 1 in 10,000 might be suffice.
+
+## Viewing metrics in Stackdriver Metrics Explorer UI
+
+The tutorial also demonstrates the recording of application metrics. Latency and transaction counts will be measured by the application code and recorded.
+
+1.  Go to the [Stackdriver Monitoring console](https://cloud.google.com/console/monitoring).
+    
+1.  For metrics, there is no sampling. Every recorded value is included in the metric representations.
+
+    ![Screenshot](./images/metrics-api-requests.png)
+
+
+## Cleaning up
+
+Since this tutorial uses multiple GCP components, please be sure to delete the associated resources once you are done.
+
+## Next steps
+
+What you saw in this tutorial is just the tip of the iceberg when it comes to the instrumentation possibilities with OpenCensus. For more details on how to implement observability in your applications, visit [https://opencensus.io](https://opencensus.io).
---
+++ tutorials/spanner-oc/java/src/main/java/com/example/spanner/App.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.spanner;
+
+import com.google.api.gax.longrunning.OperationFuture;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.spanner.Database;
+import com.google.cloud.spanner.DatabaseAdminClient;
+import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.Key;
+import com.google.cloud.spanner.Mutation;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerException;
+import com.google.cloud.spanner.SpannerExceptionFactory;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.spanner.admin.database.v1.CreateDatabaseMetadata;
+import io.opencensus.common.Scope;
+import io.opencensus.contrib.grpc.metrics.RpcViews;
+import io.opencensus.exporter.stats.stackdriver.StackdriverStatsExporter;
+import io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
+import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
+import io.opencensus.trace.Tracing;
+import io.opencensus.trace.config.TraceConfig;
+import io.opencensus.trace.samplers.Samplers;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * This sample demonstrates how to enable opencensus tracing and stats in cloud spanner client.
+ */
+public class App {
+
+  // [START configChanges]
+  private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
+  private static final String INSTANCE_ID = System.getenv( "INSTANCE_ID");
+  private static final String DATABASE_ID = System.getenv( "DATABASE_ID");
+  // [END configChanges]
+
+  // [START config_oc_stackdriver_export]
+  private static void configureOpenCensusExporters() throws IOException {
+    TraceConfig traceConfig = Tracing.getTraceConfig();
+
+    // default sampler is set to Samplers.alwaysSample() for demonstration. In production
+    // or in high QPS environment please use default sampler.
+    traceConfig.updateActiveTraceParams(
+      traceConfig.getActiveTraceParams().toBuilder().setSampler(Samplers.alwaysSample()).build());
+
+    // Create the Stackdriver trace exporter
+    StackdriverTraceExporter.createAndRegister(
+      StackdriverTraceConfiguration.builder()
+        .setProjectId(PROJECT_ID)
+        .build());
+
+    // Create the Stackdriver monitoring exporter
+    StackdriverStatsExporter.createAndRegister();
+
+    // [END config_oc_stackdriver_export]
+
+    // -------------------------------------------------------------------------------------------
+    // Register all the gRPC views
+    // OC will automatically go and instrument gRPC. It's going to capture app level metrics
+    // like latency, req/res bytes, count of req/res messages, started rpc etc.
+    // -------------------------------------------------------------------------------------------
+    RpcViews.registerAllGrpcViews();
+  }
+
+  /**
+   * Connects to Cloud Spanner, runs some basic operations.
+   */
+  private static void doSpannerOperations() {
+    // Instantiate the client.
+    SpannerOptions options = SpannerOptions.getDefaultInstance();
+    Spanner spanner = options.getService();
+
+    try {
+      DatabaseId db = DatabaseId.of(
+        options.getProjectId(), INSTANCE_ID, DATABASE_ID);
+      // And then create the Spanner database client.
+      DatabaseClient dbClient = spanner.getDatabaseClient(db);
+      DatabaseAdminClient dbAdminClient = spanner.getDatabaseAdminClient();
+
+      createDatabase(dbAdminClient, db);
+
+      try (Scope ss = Tracing.getTracer().spanBuilder("create-players").startScopedSpan()) {
+        // Warm up the spanner client session. In normal usage
+        // you'd have hit this point after the first operation.
+        dbClient.singleUse().readRow("Players", Key.of("foo@gmail.com"),
+          Collections.singletonList("email"));
+
+        // [START spanner_insert_data]
+        for (int i = 0; i < 3; i++) {
+          String up = i + "-" + (System.currentTimeMillis() / 1000) + ".";
+          List<Mutation> mutations = Arrays.asList(
+            playerMutation("Poke", "Mon", up + "poke.mon@example.org",
+              "f1578551-eb4b-4ecd-aee2-9f97c37e164e"),
+            playerMutation("Go", "Census", up + "go.census@census.io",
+              "540868a2-a1d8-456b-a995-b324e4e7957a"),
+            playerMutation("Quick", "Sort", up + "q.sort@gmail.com",
+              "2b7e0098-a5cc-4f32-aabd-b978fc6b9710")
+          );
+
+          // write to Spanner
+          dbClient.write(mutations);
+        }
+        // [END spanner_insert_data]
+      }
+    } catch (Exception e) {
+      System.out.print("Exception while adding player: " + e);
+    } finally {
+      // Closes the client which will free up the resources used
+      spanner.close();
+    }
+  }
+
+  // [START spanner_create_database]
+  private static void createDatabase(DatabaseAdminClient dbAdminClient, DatabaseId id) {
+    OperationFuture<Database, CreateDatabaseMetadata> op =
+      dbAdminClient.createDatabase(
+        id.getInstanceId().getInstance(),
+        id.getDatabase(),
+        Collections.singletonList(
+          "CREATE TABLE Players (\n"
+            + "  first_name STRING(1024),\n"
+            + "  last_name  STRING(1024),\n"
+            + "  email   STRING(1024),\n"
+            + "  uuid STRING(1024)\n"
+            + ") PRIMARY KEY (email)"));
+    try {
+      // Initiate the request which returns an OperationFuture.
+      Database db = op.get();
+      System.out.println("Created database [" + db.getId() + "]");
+    } catch (ExecutionException e) {
+      // If the operation failed during execution, expose the cause.
+      throw (SpannerException) e.getCause();
+    } catch (InterruptedException e) {
+      // Throw when a thread is waiting, sleeping, or otherwise occupied,
+      // and the thread is interrupted, either before or during the activity.
+      throw SpannerExceptionFactory.propagateInterrupt(e);
+    }
+  }
+  // [END spanner_create_database]
+
+  // [START spanner_insert_data]
+  private static Mutation playerMutation(String firstName, String lastName, String email,
+    String uuid) {
+    return Mutation.newInsertBuilder("Players")
+      .set("first_name")
+      .to(firstName)
+      .set("last_name")
+      .to(lastName)
+      .set("uuid")
+      .to(uuid)
+      .set("email")
+      .to(email)
+      .build();
+  }
+  // [END spanner_insert_data]
+
+  private static void sleep(int ms) {
+    try {
+      Thread.sleep(ms);
+    } catch (Exception ignored) { }
+  }
+
+  public static void main(String ...args) throws Exception {
+    configureOpenCensusExporters();
+    doSpannerOperations();
+
+    // IMPORTANT: do NOT exit right away. Wait for a duration longer than reporting
+    // duration (5s) to ensure spans are exported. Spans are exported every 5 seconds.
+    sleep(5100);
+  }
+}
