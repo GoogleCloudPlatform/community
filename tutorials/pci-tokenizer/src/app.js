e73d7507511d2ab839d232a0ab6b44a45e37f686
Ian Maddox <github@ianmaddox.com>
2019-12-26 12:13:58 -0800

    Lint fixes

--- tutorials/pci-tokenizer/src/app.js
+++ tutorials/pci-tokenizer/src/app.js
@@ -17,50 +17,49 @@ limitations under the License.
 /* jslint node: true */
 /* jshint esversion: 8 */
 'use strict';
-
+var path = require('path');
 /* Required libraries and supporting vars */
-process.env.NODE_CONFIG_DIR = process.env.NODE_CONFIG_DIR || __dirname + '/../config/';
+process.env.NODE_CONFIG_DIR = process.env.NODE_CONFIG_DIR || path.join(__dirname, '/../config/');
 const config = require('config');
 const DLP = require('@google-cloud/dlp');
 const dlp = new DLP.DlpServiceClient();
-const DLP_ALPHABET = "ALPHA_NUMERIC";
 const DLP_REGEX = /c(\d+)m(\d+)y(\d+)u(.*)/;
 const DLP_SURROGATE_TYPE = 'TOKEN';
 const DLP_CUSTOM_INFO_TYPE = [{
   infoType: { name: DLP_SURROGATE_TYPE },
-  regex: { pattern:".*" },
-  "likelihood" : "VERY_LIKELY"
+  regex: { pattern: '.*' },
+  likelihood: 'VERY_LIKELY'
 }];
 
 /* Project variables */
 // See config/default.json for more information
-const PROJECT_ID = config.get("general.project_id");
-const DEBUG_LOGGING = config.get("logging.debug");
-const VERSION_LOGGING = config.get("logging.version");
+const PROJECT_ID = config.get('general.project_id');
+const DEBUG_LOGGING = config.get('logging.debug');
+const VERSION_LOGGING = config.get('logging.version');
 
-var tmp_dlp_key = {};
-if(config.get("dlp.wrapped_key_resource_id") && config.get("dlp.wrapped_key")) {
-  tmp_dlp_key = {
-    "kmsWrapped": {
-      wrappedKey: config.get("dlp.wrapped_key"),
-      cryptoKeyName: config.get("dlp.wrapped_key_resource_id")
+var tmpDlpKey = {};
+if (config.get('dlp.wrapped_key_resource_id') && config.get('dlp.wrapped_key')) {
+  tmpDlpKey = {
+    kmsWrapped: {
+      wrappedKey: config.get('dlp.wrapped_key'),
+      cryptoKeyName: config.get('dlp.wrapped_key_resource_id')
     }
   };
-  debug("Using key wrapped by", tmp_dlp_key.kmsWrapped.cryptoKeyName)
+  debug('Using key wrapped by', tmpDlpKey.kmsWrapped.cryptoKeyName);
 } else {
-  tmp_dlp_key = {"unwrapped":{ "key": config.get("dlp.unwrapped_key").toString('utf8') }};
-  debug("Using unwrapped key of", Buffer.byteLength(config.get("dlp.unwrapped_key").toString('utf8'), 'utf8'), 'bytes')
+  tmpDlpKey = { unwrapped: { key: config.get('dlp.unwrapped_key').toString('utf8') } };
+  debug('Using unwrapped key of', Buffer.byteLength(config.get('dlp.unwrapped_key').toString('utf8'), 'utf8'), 'bytes');
 }
-const DLP_KEY = tmp_dlp_key;
+const DLP_KEY = tmpDlpKey;
 
 /* Helpful init stuff */
-const appVersion = require('fs').statSync(__filename).mtimeMs + " env:" + process.env.NODE_ENV;
+const appVersion = require('fs').statSync(__filename).mtimeMs + ' env:' + process.env.NODE_ENV;
 // This log line is helpful to keep track of which version of the tokenizer is running.
 // The timestamp of index.js is logged at initial script launch as well as each
 // individual execution of tokenize() and detokenize()
 logVersion(`Tokenizer LAUNCHED v.${appVersion}`);
 
-///////////////////////  DLP DLP DLP ///////////////////////
+/// ////////////////////  DLP DLP DLP ///////////////////////
 /**
  * Accepts the following params as an HTTP POST:
  *  project_id - The project ID associated with the auth_token
@@ -78,10 +77,10 @@ logVersion(`Tokenizer LAUNCHED v.${appVersion}`);
 exports.dlp_crypto_tokenize = async (req, res) => {
   logVersion(`DLP tokenizing with tokenizer v.${appVersion}`);
   // The value contains a JSON document representing the entity we want to save
-  var cc = req.body.cc + "";
-  var mm = req.body.mm + "";
-  var yyyy = req.body.yyyy + "";
-  var userid = req.body.user_id + "";
+  var cc = req.body.cc + '';
+  var mm = req.body.mm + '';
+  var yyyy = req.body.yyyy + '';
+  var userid = req.body.user_id + '';
   var projectId = req.body.project_id || PROJECT_ID || process.env.GCP_PROJECT;
 
   if (!projectId) {
@@ -92,20 +91,20 @@ exports.dlp_crypto_tokenize = async (req, res) => {
     return res.status(500).send('Invalid input for CC');
   }
 
-  if(!mm || mm < 0 || mm > 12) {
+  if (!mm || mm < 0 || mm > 12) {
     return res.status(500).send('Invalid input for mm');
   }
 
-  if(!yyyy || yyyy < 2010 || yyyy > 3000) {
+  if (!yyyy || yyyy < 2010 || yyyy > 3000) {
     return res.status(500).send('Invalid input for yyyy');
   }
 
-  if(!userid || userid.length < 4) {
+  if (!userid || userid.length < 4) {
     return res.status(500).send('Invalid input for user_id');
   }
 
   try {
-    let request = {
+    const request = {
       parent: dlp.projectPath(projectId),
       inspectConfig: {
         // infoTypes: [{name: 'DATE'}, {name: 'CREDIT_CARD_NUMBER'}],
@@ -118,33 +117,32 @@ exports.dlp_crypto_tokenize = async (req, res) => {
           transformations: [
             {
               primitiveTransformation: {
-                "cryptoDeterministicConfig":{
-                  "cryptoKey": DLP_KEY,
+                cryptoDeterministicConfig: {
+                  cryptoKey: DLP_KEY,
                   surrogateInfoType: {
-                    name: DLP_SURROGATE_TYPE,
+                    name: DLP_SURROGATE_TYPE
                   }
-                },
-              },
-            },
-          ],
-        },
+                }
+              }
+            }
+          ]
+        }
       },
-      item: {value:String(`c${cc}m${mm}y${yyyy}u${userid}`)}
+      item: { value: String(`c${cc}m${mm}y${yyyy}u${userid}`) }
     };
     // Run deidentification request
     const [response] = await dlp.deidentifyContent(request);
-    const token = response.item.value.replace(/^.*:/,''); // Use regex to deconstruct DLP framing
-    if(response.overview.transformationSummaries[0].results[0].code == "ERROR") {
+    const token = response.item.value.replace(/^.*:/, ''); // Use regex to deconstruct DLP framing
+    if (response.overview.transformationSummaries[0].results[0].code === 'ERROR') {
       return res.status(500).send(response);
     } else {
       return res.status(200).send(token);
     }
-  }
-  catch(err) {
+  } catch (err) {
     res.status(500).send(`Error during tokenization: ${err.message}.`);
     return false;
   }
-}
+};
 
 /**
  * Accepts the following params as an HTTP POST:
@@ -159,10 +157,9 @@ exports.dlp_crypto_tokenize = async (req, res) => {
  * @param {object} res - tokenizer response object
  */
 exports.dlp_crypto_detokenize = async (req, res) => {
-let i = 0;
   logVersion(`DLP detokenizing ver ${appVersion}`);
-  var ccToken = req.body.token + "";
-  var userid = req.body.user_id + "";
+  var ccToken = req.body.token + '';
+  var userid = req.body.user_id + '';
   var projectId = req.body.project_id || PROJECT_ID || process.env.GCP_PROJECT;
 
   if (!projectId) {
@@ -173,7 +170,7 @@ let i = 0;
     throw new Error('Invalid input for token: ' + ccToken);
   }
 
-  if(!userid || userid.length < 4) {
+  if (!userid || userid.length < 4) {
     return res.status(500).send('Invalid input for user_id');
   }
 
@@ -181,58 +178,57 @@ let i = 0;
 
   try {
     const request = {
-        parent: dlp.projectPath(projectId),
-        inspectConfig: {
-          includeQuote: false,
-          customInfoTypes: DLP_CUSTOM_INFO_TYPE,
-        },
-        reidentifyConfig: {
-          infoTypeTransformations: {
-            transformations: [
-              {
-                infoTypes: [{ name:DLP_SURROGATE_TYPE }],
-                primitiveTransformation: {
-                  cryptoDeterministicConfig:{
-                    cryptoKey: DLP_KEY,
-                    surrogateInfoType: {
-                      name: DLP_SURROGATE_TYPE,
-                    },
-                  },
-                },
-              },
-            ],
-          },
-        },
-        // item: {value: DLP_SURROGATE_TYPE+`(${ccToken.length}):${ccToken}`},
-        item: {value: ccToken},
-      };
+      parent: dlp.projectPath(projectId),
+      inspectConfig: {
+        includeQuote: false,
+        customInfoTypes: DLP_CUSTOM_INFO_TYPE
+      },
+      reidentifyConfig: {
+        infoTypeTransformations: {
+          transformations: [
+            {
+              infoTypes: [{ name: DLP_SURROGATE_TYPE }],
+              primitiveTransformation: {
+                cryptoDeterministicConfig: {
+                  cryptoKey: DLP_KEY,
+                  surrogateInfoType: {
+                    name: DLP_SURROGATE_TYPE
+                  }
+                }
+              }
+            }
+          ]
+        }
+      },
+      // item: {value: DLP_SURROGATE_TYPE+`(${ccToken.length}):${ccToken}`},
+      item: { value: ccToken }
+    };
 
-      const [response] = await dlp.reidentifyContent(request);
+    const [response] = await dlp.reidentifyContent(request);
 
-      if(!response || response === undefined || response.overview.transformationSummaries[0].results[0].code == "ERROR") {
-        return res.status(500).send(response);
-      }
+    if (!response || response === undefined || response.overview.transformationSummaries[0].results[0].code === 'ERROR') {
+      return res.status(500).send(response);
+    }
 
-      let rawDetok = response.item.value.toString();
-      let detok = DLP_REGEX.exec(rawDetok);
+    const rawDetok = response.item.value.toString();
+    const detok = DLP_REGEX.exec(rawDetok);
 
-      if(detok.length != 5) {
-        return res.status(500).send("Invalid decoded block size of " + detok.length);
-      }
+    if (detok.length !== 5) {
+      return res.status(500).send('Invalid decoded block size of ' + detok.length);
+    }
 
-      if(detok[4] !== userid) {
-        return res.status(500).send("Could not validate detokenized content");
-      }
+    if (detok[4] !== userid) {
+      return res.status(500).send('Could not validate detokenized content');
+    }
 
-      let out = {
-        cc: detok[1],
-        mm: detok[2],
-        yyyy: detok[3]
-      };
+    const out = {
+      cc: detok[1],
+      mm: detok[2],
+      yyyy: detok[3]
+    };
 
-      res.status(200).send(out);
-  }
-  catch(err) {
+    res.status(200).send(out);
+  } catch (err) {
     res.status(500).send(`DLP Detokenization error: ${err.message}`);
     return false;
   }
@@ -241,16 +237,15 @@ let i = 0;
 /**
  * Helpful debug function that checks for the var DEBUG_LOGGING == true before writing to console.log()
  */
-function debug(...args) {
-  if(DEBUG_LOGGING) console.log(...args);
+function debug (...args) {
+  if (DEBUG_LOGGING) console.log(...args);
 }
-
 
 /**
  * A handy utility function that can write the timestamp of app.js when the tokenizer
  * is first launched and again each time it is executed. This is helpful when trying
  * to link log output of a particular run to the code version that generated it.
  */
-function logVersion(...args) {
-  if(VERSION_LOGGING) console.log(...args);
+function logVersion (...args) {
+  if (VERSION_LOGGING) console.log(...args);
 }
