56caf95a1e794c4d8ff05fb634e8b90e7f1a1c15
Ian Maddox <github@ianmaddox.com>
2019-12-26 12:35:59 -0800

    Using DLP and Cloud Run (#1097)
    * Using DLP and Cloud Run
    
    * Using DLP and Cloud Run
    This is a major rewrite of the tokenizer to enable Anthos and Cloud Run execution.
    The tokenizer is also now backed by Cloud DLP and uses cryptoDeterministicEncryption.
    
    * minor copy-edits
    
    * minor copy-edits
    
    * minor copy-edits
    
    * copy-editing and production pass
    
    * Lint fixes
    
    * Lint fixes
    
    * Update envvars
    
    * updating relative links to work from cloud.google.com
    
    Co-authored-by: Todd Kopriva <43478937+ToddKopriva@users.noreply.github.com>

--- tutorials/pci-tokenizer/src/deploy-detokenize.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Utility script to streamline CF deployment
-# See ../README.md for license and more info
-
-SRC=$(dirname "`dirname \"$BASH_SOURCE\"/`")
-gcloud functions deploy detokenize --runtime=nodejs8 --trigger-http --entry-point=detokenize --memory=256MB --source=$SRC
