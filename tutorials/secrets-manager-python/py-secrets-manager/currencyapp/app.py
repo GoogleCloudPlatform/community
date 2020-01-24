6aba2d91637da50594b724a0b7c191469250ba80
Jason (Jay) Smith <jaysmith@google.com>
2020-01-24 12:14:11 -0600

    Created a tutorial on using Secret Manager with Python in Cloud Run (#1120)
    * Cloud Run Eventing
    
    * fixed index.md issue
    
    * frontmatter added
    
    * frontmatter added
    
    * frontmatter added try 3
    
    * frontmatter added try 4
    
    * frontmatter added try 5
    
    * Knative Eventing 0.10 upgrade and Kafka Broker
    
    * fixed topic creation
    
    * cleaned up variable
    
    * remove deploy
    
    * Broker
    
    * Kafka Knative Broker
    
    * for gitlab
    
    * git lab cleanup
    
    * added display
    
    * Removed License
    
    * first editing and production pass
    
    This is the first editing and production pass. I'll follow behind with another pass in a moment.
    
    * a couple of final copy-edits
    
    * Initial commit of my tutorial
    
    * Updated tutorial documentation on the code
    
    * Fixed description
    
    * still fixing description
    
    * halfway through edit
    
    * continuing edit
    
    * done with first editing pass
    
    Now that I've finished the first editing pass, I'll go through and test the steps and refine the edit. I still need to add a section for cleaning up resources.
    
    * added step to get code, added links to code
    
    * adding information about project and Cloud Shell
    
    Co-authored-by: Todd Kopriva <43478937+ToddKopriva@users.noreply.github.com>

---
+++ tutorials/secrets-manager-python/py-secrets-manager/currencyapp/app.py
@@ -0,0 +1,37 @@
+import os
+import json
+import pandas
+
+from flask import Flask, jsonify, redirect, render_template, request
+
+from google.cloud import secretmanager
+
+from alpha_vantage.timeseries import TimeSeries
+
+
+app = Flask(__name__)
+
+PROJECT_ID = os.environ.get("PROJECTID")
+
+secrets = secretmanager.SecretManagerServiceClient()
+ALPHA_VANTAGE_KEY = secrets.access_secret_version("projects/"+PROJECT_ID+"/secrets/alpha-vantage-key/versions/1").payload.data.decode("utf-8")
+
+ts = TimeSeries(key=ALPHA_VANTAGE_KEY)
+
+@app.route("/")
+def hello():
+    return "Hello World!!!"
+
+
+
+@app.route('/api/v1/symbol', methods=['POST'])
+def get_time_series():
+    if request.method == 'POST':
+        symbol = request.args['symbol']
+        data, metadata = ts.get_intraday(
+                symbol, interval='15min', outputsize="25")
+    return jsonify(data=data)
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
