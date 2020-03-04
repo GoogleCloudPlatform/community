9b4910458e54320a95900e8ecd1fba199ed7bd78
Mikolaj Kucharski <mikolaj@kucharski.name>
2020-03-04 22:12:53 +0000

    Explicitly depends on `google_project_service.service` resource
    Make it simple and use explicit dependency on resource which
    enables APIs. I think this is more clear.

--- tutorials/managing-gcp-projects-with-terraform/compute.tf
+++ tutorials/managing-gcp-projects-with-terraform/compute.tf
@@ -17,7 +17,7 @@ data "google_compute_zones" "available" {
 }
 
 resource "google_compute_instance" "default" {
-  project      = google_project_service.service["compute.googleapis.com"].project
+  project      = google_project.project.project_id
   zone         = data.google_compute_zones.available.names[0]
   name         = "tf-compute-1"
   machine_type = "f1-micro"
@@ -32,6 +32,8 @@ resource "google_compute_instance" "default" {
     network = "default"
     access_config {}
   }
+
+  depends_on = [google_project_service.service]
 }
 
 output "instance_id" {
