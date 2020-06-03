95aabc7423ab3b0460ec3c8ec544e9c5e540fe1d
Gal Zahavi <galz100@gmail.com>
2020-06-03 09:13:58 -0700

    Added Licenses

--- tutorials/embedded-c-getting-started/examples/main/component.mk
+++ tutorials/embedded-c-getting-started/examples/main/component.mk
@@ -1,11 +1,17 @@
-#
-# Main component makefile.
-#
-# This Makefile can be left empty. By default, it will take the sources in the 
-# src/ directory, compile them and link them into lib(subdirectory_name).a 
-# in the build directory. This behaviour is entirely configurable,
-# please read the ESP-IDF documents if you need to do this.
-#
+#/******************************************************************************
+#* Copyright 2020 Google
+#* Licensed under the Apache License, Version 2.0 (the "License");
+#* you may not use this file except in compliance with the License.
+#* You may obtain a copy of the License at
+#*
+#*    http://www.apache.org/licenses/LICENSE-2.0
+#*
+#* Unless required by applicable law or agreed to in writing, software
+#* distributed under the License is distributed on an "AS IS" BASIS,
+#* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#* See the License for the specific language governing permissions and
+#* limitations under the License.
+#*****************************************************************************/
 COMPONENT_EMBED_TXTFILES := certs/private_key.pem
 
 ifndef IDF_CI_BUILD
