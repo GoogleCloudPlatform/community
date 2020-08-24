f28ec2bf69df1bca89427f194f9e7ae5a1fbe64a
zhong <zhongchen@google.com>
2020-08-24 15:09:37 -0700

    add dummy logic for dataflow job

--- tutorials/schedule-dataflow-jobs-with-cloud-scheduler/scheduler-dataflow-demo/dataflow/src/main/java/DataflowDemoPipeline.java
+++ tutorials/schedule-dataflow-jobs-with-cloud-scheduler/scheduler-dataflow-demo/dataflow/src/main/java/DataflowDemoPipeline.java
@@ -1,9 +1,19 @@
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.PDone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class DataflowDemoPipeline {
-    public static void main(String[] args){
+    private static final Logger Log = LoggerFactory.getLogger(DataflowDemoPipeline.class);
 
+    public static void main(String[] args) {
         // Register Options class for our pipeline with the factory
         PipelineOptionsFactory.register(DemoPipelineOptions.class);
 
@@ -13,6 +23,20 @@ public class DataflowDemoPipeline {
 
         Pipeline p = Pipeline.create(options);
 
+        String[] numbers = new String[]{"one", "two", "three"};
+        List<String> input = Arrays.asList(numbers);
+        p.apply("dummy input", Create.of(input)).
+                apply("dummy transformation", ParDo.of(
+                        new DoFn<String, String>() {
+                            @ProcessElement
+                            public void processElement(ProcessContext context) {
+                                String elem = context.element();
+                                Log.info("Process element: " + elem);
+                                context.output(elem);
+                            }
+                        }
+                ));
+        PDone.in(p);
         p.run();
     }
 }
