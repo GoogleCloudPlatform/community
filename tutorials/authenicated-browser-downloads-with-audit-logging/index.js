83a69c588b2a9e3f174619dfdcda905c05a38570
Ian Maddox <ianmaddox@google.com>
2020-09-15 15:59:02 -0700

    Lint fixes

--- tutorials/authenicated-browser-downloads-with-audit-logging/index.js
+++ tutorials/authenicated-browser-downloads-with-audit-logging/index.js
@@ -3,7 +3,7 @@
  WEB  =>  IAP =>  APP Engine  =>  Cloud Storage
 */
 
-const {Storage} = require('@google-cloud/storage');
+const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
 const mime = require('mime/lite');
 const express = require('express');
@@ -15,22 +15,22 @@ app.listen(PORT, () => {
 });
 
 app.get('*', (req, res) => {
-  let fileName = req.originalUrl.substring(1);
-  let fileExt = fileName.match(/\.([^\.]+)$/);
-  if(!fileExt || !fileExt[1] || !fileName) {
-      res.status(400).send('<h1>400 Bad request.</h1>').end();
-      return;
+  const fileName = req.originalUrl.substring(1);
+  const fileExt = fileName.match(/\.([^\.]+)$/);
+  if (!fileExt || !fileExt[1] || !fileName) {
+    res.status(400).send('<h1>400 Bad request.</h1>').end();
+    return;
   }
-  let fileMime = mime.getType(fileExt[1]);
-  const bucketName = process.env.GCS_BUCKET || "tw-aaa-assets";
+  const fileMime = mime.getType(fileExt[1]);
+  const bucketName = process.env.GCS_BUCKET || 'tw-aaa-assets';
   const bucket = storage.bucket(bucketName);
   const file = bucket.file(fileName);
-  file.download(function(err, contents) {
-    if(err) {
+  file.download(function (err, contents) {
+    if (err) {
       res.status(404).send('<h1>404 File not found.</h1>').end();
     } else {
-      res.setHeader("Content-Type", fileMime);
-      res.setHeader('Content-Disposition','inline');
+      res.setHeader('Content-Type', fileMime);
+      res.setHeader('Content-Disposition', 'inline');
       res.status(200).send(contents).end();
     }
   });
