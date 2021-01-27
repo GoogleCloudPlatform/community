72b199b738359fa77366a26cf6118a6eb605febe
Ian Maddox <ianmaddox@google.com>
2021-01-26 21:05:28 -0800

    Convert clamav scanner from clamscan to clamdscan (#1598)
    * Converting to only use clamdscan
    
    * Checking for config file before deploying
    
    * edit pass
    
    Co-authored-by: Ian Maddox <github@tiz.io>
    Co-authored-by: Todd Kopriva <43478937+ToddKopriva@users.noreply.github.com>

--- tutorials/gcp-cos-clamav/start.py
+++ tutorials/gcp-cos-clamav/start.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
 import os
+from os import path
+
+# Init the configs so they can be maintained from outside the container
+def init_conf(conffile):
+    if not os.path.exists(f"/etc/clamav/{conffile}"):
+        print(f"Initializing conf file {conffile}")
+        os.system(f"cp /etc/clamav-custom/{conffile} /etc/clamav/{conffile}")
+
+init_conf("clamd.conf")
+init_conf("freshclam.conf")
+if os.path.exists("/tmp/clamscan.lock"):
+    print("Cleaning up lock file from bad shutdown")
+    os.remove("/tmp/clamscan.lock")
 
 # Schedule a filesystem scan every hour unless one is in progress.
 os.system("echo \"$(($RANDOM % 60))   *   *   *   *   /scan.sh > /proc/1/fd/1 2>&1\" > /root.crontab")
