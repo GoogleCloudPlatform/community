190af666fe07fcc20a254c9ee52e7053e013144b
Sandeep Parmar <livesankp@gmail.com>
2021-03-17 15:00:22 -0500

    Update env.template.js

--- tutorials/appengine-angular-nginx-docker/sample-app/src/assets/env.template.js
+++ tutorials/appengine-angular-nginx-docker/sample-app/src/assets/env.template.js
@@ -1,7 +1,7 @@
 (function (window) {
   window.env = window.env || {};
-  //Environment variable
-  //window['env']['apiurl'] = '${API_URL}';
-  //doing below work around for lint issue and replacing above line
+  // Environment variable
+  // window['env']['apiurl'] = '${API_URL}';
+  // doing below work around for lint issue and replacing above line
   window['env']['apiurl'] = `${''}\${API_URL}`;
 })(this);
