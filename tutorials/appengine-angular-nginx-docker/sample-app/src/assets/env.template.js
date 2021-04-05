9ae1a9fd947d0ff31b925def5ebe8be6656f3622
Sandeep Parmar <sandeepkumar.parmar@davita.com>
2021-04-04 21:02:19 -0500

    File name updates
    Updated file names to make it more understanding

--- tutorials/appengine-angular-nginx-docker/sample-app/src/assets/env.template.js
+++
@@ -1,7 +0,0 @@
-(function (window) {
-  window.env = window.env || {};
-  // Environment variable
-  // window['env']['apiurl'] = '${API_URL}';
-  // doing below work around for lint issue and replacing above line
-  window['env']['apiurl'] = `${''}\${API_URL}`;
-})(this);
