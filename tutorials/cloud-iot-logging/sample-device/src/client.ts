2701c4419f4dc6ba2141b695d7579e66f88c337e
xiangshen-dk <shenxiang@google.com>
2021-07-09 19:23:08 -0400

    [Update] Using Cloud Logging with IoT Core devices (#1879)
    * update code for the tutorial
    
    * add step to clone the repo

--- tutorials/cloud-iot-logging/sample-device/src/client.ts
+++ tutorials/cloud-iot-logging/sample-device/src/client.ts
@@ -14,86 +14,95 @@
 # limitations under the License.
 */
 
-
 import mqtt = require('mqtt');
-import { TokenGenerator, SignAlgorithm } from "./token";
-import { setInterval } from 'timers';
-import { Observable } from 'rxjs';
-import 'rxjs/add/observable/fromEvent';
+import {TokenGenerator, SignAlgorithm} from './token';
+import {setInterval} from 'timers';
+import {Observable} from 'rxjs';
 import {ReplaySubject} from 'rxjs';
 
 export class IoTClient {
-    private jwt: string;
-    private tokenSource: TokenGenerator;
-    private client: mqtt.MqttClient;
+  private jwt: string | undefined;
+  private tokenSource: TokenGenerator;
+  private client: mqtt.MqttClient;
 
-    connections$: ReplaySubject<mqtt.IConnackPacket>;
-    // tslint:disable-next-line: no-any
-    disconnections$: ReplaySubject<any>;
-    // tslint:disable-next-line: no-any
-    messages$: ReplaySubject<any>;
-    // tslint:disable-next-line: no-any
-    publishConfirmations$: ReplaySubject<any>;
+  connections$: ReplaySubject<mqtt.IConnackPacket>;
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  disconnections$: ReplaySubject<any>;
+  messages$: ReplaySubject<any>;
+  publishConfirmations$: ReplaySubject<any>;
 
-    constructor (
-        private projectId: string,
-        private region: string,
-        private registryId: string,
-        private deviceId: string,
-        private privateKeyFile: string,
-        private algorithm: SignAlgorithm = SignAlgorithm.ES256,
-        private port = 8883,
-        private tokenRefreshMinutes = 60,
-        ) { 
-            this.tokenSource = new TokenGenerator(this.projectId, this.privateKeyFile, this.algorithm);
-            // this.connections$ = Observable.create();
-            this.connections$ = new ReplaySubject(1);
-            this.disconnections$ = new ReplaySubject(1);
-            this.messages$ = new ReplaySubject(1);
-            this.publishConfirmations$ = new ReplaySubject(1);
-            // use expiration period set in constructor
-            this.refresh();
+  constructor(
+    private projectId: string,
+    private region: string,
+    private registryId: string,
+    private deviceId: string,
+    private privateKeyFile: string,
+    private algorithm: SignAlgorithm = SignAlgorithm.ES256,
+    private port = 8883,
+    private tokenRefreshMinutes = 60
+  ) {
+    this.tokenSource = new TokenGenerator(
+      this.projectId,
+      this.privateKeyFile,
+      this.algorithm
+    );
+    // this.connections$ = Observable.create();
+    this.connections$ = new ReplaySubject(1);
+    this.disconnections$ = new ReplaySubject(1);
+    this.messages$ = new ReplaySubject(1);
+    this.publishConfirmations$ = new ReplaySubject(1);
+    // use expiration period set in constructor
+    this.refresh();
+  }
+  refresh() {
+    this.jwt = this.tokenSource.create();
+    if (this.client) {
+      this.client.end();
+    }
+    this.connect();
+    setTimeout(
+      this.refresh.bind(this),
+      this.tokenRefreshMinutes * 60 * 1000 - 60000
+    );
+  }
+  connect() {
+    const connectionArgs = {
+      host: 'mqtt.googleapis.com',
+      port: this.port,
+      clientId: `projects/${this.projectId}/locations/${this.region}/registries/${this.registryId}/devices/${this.deviceId}`,
+      username: 'unused',
+      password: this.jwt,
+      protocol: 'mqtts',
+      secureProtocol: 'TLSv1_2_method',
+    };
+    this.client = mqtt.connect(connectionArgs);
+
+    // tslint:disable-next-line: no-any
+    this.client.on(
+      'message',
+      (topic: string, message: any, packet: mqtt.IPacket) => {
+        this.messages$.next(message);
+      }
+    );
+
+    this.client.on('connect', (connack: mqtt.IConnackPacket) => {
+      this.client.subscribe(
+        `/devices/${this.deviceId}/config`,
+        (err, granted) => {
+          if (err) {
+            console.error('subscription failed');
+          }
         }
-    refresh() {
-        this.jwt = this.tokenSource.create();
-        if (this.client) {
-            this.client.end();
-        }
-        this.connect();
-        setTimeout(this.refresh.bind(this), (this.tokenRefreshMinutes * 60 * 1000) - 60000);
-    }
-    connect() {
+      );
+      this.connections$.next(connack);
+    });
 
-        const connectionArgs = {
-            host: 'mqtt.googleapis.com',
-            port: this.port,
-            clientId: `projects/${this.projectId}/locations/${this.region}/registries/${this.registryId}/devices/${this.deviceId}`,
-            username: 'unused',
-            password: this.jwt,
-            protocol: 'mqtts',
-            secureProtocol: 'TLSv1_2_method'
-          };
-        this.client = mqtt.connect(connectionArgs);
+    this.client.on('error', error => console.error(error));
+  }
 
-        // tslint:disable-next-line: no-any
-        this.client.on('message', (topic:string, message:any, packet:mqtt.IPacket) =>{
-            this.messages$.next(message);
-        });
-
-        this.client.on('connect', (connack:mqtt.IConnackPacket) => {
-            this.client.subscribe(`/devices/${this.deviceId}/config`, (err, granted) => {
-                if (err) {
-                    console.error("subscription failed");
-                }
-            });
-            this.connections$.next(connack);
-        });
-            
-        this.client.on('error', (error) =>
-            console.error(error));
-    }
-    
-    publish(topic: string, payload: string | Buffer) {
-        this.client.publish(topic, payload, (ack) => this.publishConfirmations$.next(ack));
-    }
+  publish(topic: string, payload: string | Buffer) {
+    this.client.publish(topic, payload, ack =>
+      this.publishConfirmations$.next(ack)
+    );
+  }
 }
