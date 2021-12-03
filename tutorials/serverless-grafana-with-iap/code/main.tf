5ccc96da03195bdaf6d94e7aa9f372bfad423b92
cstanger <cstanger@google.com>
2021-12-03 17:02:34 +0000

    cleanup code

--- tutorials/serverless-grafana-with-iap/code/main.tf
+++ tutorials/serverless-grafana-with-iap/code/main.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 locals {
   apis = ["iam.googleapis.com", "compute.googleapis.com", "run.googleapis.com", "apigateway.googleapis.com", "servicemanagement.googleapis.com", "servicecontrol.googleapis.com", "iap.googleapis.com", "sql-component.googleapis.com", "cloudapis.googleapis.com", "sqladmin.googleapis.com", "secretmanager.googleapis.com", "cloudresourcemanager.googleapis.com"]
 }
@@ -178,7 +194,10 @@ resource "google_secret_manager_secret_iam_member" "datasource-access" {
   secret_id  = google_secret_manager_secret.datasource.id
   role       = "roles/secretmanager.secretAccessor"
   member     = "serviceAccount:${google_service_account.grafana_sa.email}"
-  depends_on = [google_secret_manager_secret.datasource, google_secret_manager_secret_version.datasource-version-data]
+  depends_on = [
+    google_secret_manager_secret.datasource, 
+    google_secret_manager_secret_version.datasource-version-data
+  ]
 }
 
 
@@ -204,7 +223,10 @@ resource "google_secret_manager_secret_iam_member" "dashboard-yaml-access" {
   secret_id  = google_secret_manager_secret.dashboard-yaml.id
   role       = "roles/secretmanager.secretAccessor"
   member     = "serviceAccount:${google_service_account.grafana_sa.email}"
-  depends_on = [google_secret_manager_secret.dashboard-yaml, google_secret_manager_secret_version.dashboard-yaml-version-data]
+  depends_on = [
+    google_secret_manager_secret.dashboard-yaml, 
+    google_secret_manager_secret_version.dashboard-yaml-version-data
+  ]
 }
 
 
@@ -230,5 +252,8 @@ resource "google_secret_manager_secret_iam_member" "dashboard-json-access" {
   secret_id  = google_secret_manager_secret.dashboard-json.id
   role       = "roles/secretmanager.secretAccessor"
   member     = "serviceAccount:${google_service_account.grafana_sa.email}"
-  depends_on = [google_secret_manager_secret.dashboard-json, google_secret_manager_secret_version.dashboard-json-version-data]
+  depends_on = [
+    google_secret_manager_secret.dashboard-json,
+    google_secret_manager_secret_version.dashboard-json-version-data
+  ]
 }
