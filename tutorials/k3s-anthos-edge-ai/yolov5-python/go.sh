a128cd85428c8705c8217478ba70b5ca2c367f07
kalschi <kalschi@gmail.com>
2021-12-14 08:06:21 +0800

    udpate

--- tutorials/k3s-anthos-edge-ai/yolov5-python/go.sh
+++ tutorials/k3s-anthos-edge-ai/yolov5-python/go.sh
@@ -1,2 +1,2 @@
 source ~/.bashrc
-python3 track.py --source $SOURCE --yolo_weights $WEIGHT --save-vid --save-txt --no-display --output $OUTPUT
+python3 track.py --source $SOURCE --yolo_weights $WEIGHT --save-vid --save-txt --no-display --output $OUTPUT --evaluate
