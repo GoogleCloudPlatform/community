772e8e57ba90079e3b9ccecca1358042bbc854e3
Sandeep Parmar <livesankp@gmail.com>
2022-03-13 23:58:08 -0500

    Add files via upload

---
+++ tutorials/gke-dotnet-docker-ingress/SampleApplication/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//GKE
+string port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrEmpty(port))
+{
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+}
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//GKE
+// Added after project creation for GKE health checks
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//disabled https for gke
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+//GKE
+// Added after project creation for GKE health checks
+app.UseHealthChecks("/healthz");
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
